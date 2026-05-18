494ecc36c6066f7a6a1fc8d080deefc58e3d3b62
tiwariaditi-collab <tiwri.at.aditi@gmail.com>
2026-05-18 20:35:11 +0530

    SQL DAY1 code

---
+++ MYsql_local.session.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS company;
+
+USE company;
+
+CREATE TABLE IF NOT EXISTS employees (
+  id INT,
+  name VARCHAR(50),
+  salary INT
+);
+
+INSERT INTO employees (id , name , salary) VALUES
+(1,'Aditi',50000);
+(2, 'Vikram', 60000);
+(3, 'Anjali', 63000),
+(4, 'Rohit', 58000);
+
+SELECT * FROM employees;
